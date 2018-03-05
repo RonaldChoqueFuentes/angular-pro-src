d82250390f2485c7de719357a1f784b9709eaac9
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-05 12:58:22 -0400

    reactive 42

--- 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -12,9 +12,15 @@ const COUNTER_CONTROL_ACCESSOR = {
   providers: [COUNTER_CONTROL_ACCESSOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
-    <div class="stock-counter">
+    <div class="stock-counter" 
+    [class.focused]="focus">
       <div>
-        <div>
+        <div
+         tabindex="0"
+         (keydown)="onKeyDown($event)"
+         (blur)="onBlur($event)"
+         (focus)="onFocus($event)"
+           >
           <p>{{ value }}</p>
           <div>
             <button 
@@ -57,7 +63,38 @@ export class StockCounterComponent implements ControlValueAccessor {
     @Input() min:number = 10;
     @Input() max:number = 10;
     value:number = 10;
+    focus:boolean;
+
     
+    onKeyDown(event:KeyboardEvent){
+        const handlers = { 
+            ArrowDown: ( ) => this.decrement(),
+            ArrowUp: ( ) => this.increment()
+        }
+
+        if(handlers[event.code]){
+            handlers[event.code]();
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        this.onTouch();
+    }
+
+    ofFocus(event:FocusEvent){
+       this.focus = false;
+       event.preventDefault();
+       event.stopPropagation();
+       this.onTouch();
+    }
+
+    ofBlur(event:FocusEvent){
+        this.focus = true; 
+        event.preventDefault();
+        event.stopPropagation();
+        this.onTouch(); 
+    }
+
     increment(){
 
         if(this.value < this.max){
