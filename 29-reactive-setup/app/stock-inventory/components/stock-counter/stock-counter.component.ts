e1d43ba1a1ad630f4984126cda54ed48c6dcb6a7
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-05 11:18:45 -0400

    reactive 41

--- 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,50 +1,80 @@
-import {Component,Input} from '@angular/core'
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const COUNTER_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
+
 @Component({
-selector:'stock-counter',
-styleUrls:['stock-counter.component.scss'],
-template:`
-            <div class="stock-counter">
-             <div>
-                <div>
-                <p>{{value}}</p>
-                <div>
-                <button type="button"
-                (click)="increment()" 
-                [disabled]="value === max"
-                 >
-                +
-                </button>
-                <button type="button" (click)="decrement()" 
-                [disabled]="value === min">
-                -
-                </button>
-                </div>
-                </div>
-             </div>
-            </div>
-         `
-
+  selector: 'stock-counter',
+  providers: [COUNTER_CONTROL_ACCESSOR],
+  styleUrls: ['stock-counter.component.scss'],
+  template: `
+    <div class="stock-counter">
+      <div>
+        <div>
+          <p>{{ value }}</p>
+          <div>
+            <button 
+              type="button"
+              (click)="increment()"
+              [disabled]="value === max">
+              +
+            </button>
+            <button 
+              type="button"
+              (click)="decrement()"
+              [disabled]="value === min">
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 })
-export class StockCounterComponent{
+export class StockCounterComponent implements ControlValueAccessor {
+
+    private onTouch: Function;
+    private onModelChange: Function;
+
+    writeValue(value){
+      this.value = value || 0;
+    }
+
+    registerOnTouched(fn) {
+        this.onTouch = fn;
+    }
+      
+    registerOnChange(fn) {
+        this.onModelChange = fn;
+    }
+      
+
     @Input() step:number = 10;
-
     @Input() min:number = 10;
-
     @Input() max:number = 10;
-
     value:number = 10;
     
     increment(){
-        if(this.value< this.max){
+
+        if(this.value < this.max){
             this.value = this.value + this.step;
+            this.onModelChange(this.value);
         }
-      
+
+        this.onTouch();
     }
 
     decrement(){
 
         if(this.value > this.min){
           this.value = this.value - this.step;
+          this.onModelChange(this.value);
         }
+
+        this.onTouch();
     }
 }
