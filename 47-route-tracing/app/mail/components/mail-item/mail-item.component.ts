68318ee5595898e0c67359f3fa3dc32239607686
Ronald <ron.todo.poderoso@gmail.com>
2018-03-07 20:44:59 -0400

    use route api to routelink 53

--- 47-route-tracing/app/mail/components/mail-item/mail-item.component.ts
+++ 47-route-tracing/app/mail/components/mail-item/mail-item.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
   template: `
     <a class="mail-item"
-     [routerLink] = "['',{outlets: { pane: ['message',message.id]} }]"
-     routerLinkActive = "active">
+     (Click)="navigateToMessage()">
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date:'shortTime' }}</span>
@@ -20,4 +20,11 @@ import { Mail } from '../../models/mail.interface';
 export class MailItemComponent {
   @Input()
   message: Mail;
+
+   constructor(private router:Router){}
+  navigateToMessage(){
+    this.router.navigate(
+      ['',{outlets: { pane: ['message',this.message.id]} }]
+    );
+  }
 }
