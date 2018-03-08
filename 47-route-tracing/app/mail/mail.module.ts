6ab2f58d2166b4d23a95ec3df160e93dee4c2361
Ronald <ron.todo.poderoso@gmail.com>
2018-03-07 20:21:13 -0400

    reuting 51 aux outlets

--- 47-route-tracing/app/mail/mail.module.ts
+++ 47-route-tracing/app/mail/mail.module.ts
@@ -5,9 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
 import { MailItemComponent } from './components/mail-item/mail-item.component';
 import { MailAppComponent } from './components/mail-app/mail-app.component';
+import {MailViewComponent} from './components/mail-view/mail-view.component'
 
 import { MailFolderResolve } from './containers/mail-folder/mail-folder.resolve';
 import { MailService } from './mail.service';
+
 
 export const ROUTES: Routes = [
   {
@@ -16,6 +18,11 @@ export const ROUTES: Routes = [
     resolve: {
       messages: MailFolderResolve
     }
+  },
+  {
+    path: 'message/:id',
+    component: MailViewComponent,
+    outlet:'pane'
   }
 ];
 
@@ -27,7 +34,8 @@ export const ROUTES: Routes = [
   declarations: [
     MailFolderComponent,
     MailAppComponent,
-    MailItemComponent
+    MailItemComponent,
+    MailViewComponent
   ],
   providers: [
     MailService,
