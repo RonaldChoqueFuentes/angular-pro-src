3d1928795be8c9abe51181fa20f4e31bb55eaad1
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-10 11:33:27 -0400

    preload strategy custom 58

--- 47-route-tracing/app/app.module.ts
+++ 47-route-tracing/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes, PreloadingStrategy, Route } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { MailModule } from './mail/mail.module';
 
 import { AppComponent } from './app.component';
+import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/observable/of';
+
+
+export class CustomPreload implements PreloadingStrategy{
+   preload(route:Route, fn:() => Observable<any>) : Observable<any>{
+      return route.data && route.data.preload? fn():Observable.of(null)
+   }
+}
 
 export const ROUTES: Routes = [
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'dashboard',data:{ preload:true }, loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
@@ -16,11 +26,12 @@ export const ROUTES: Routes = [
   declarations: [
     AppComponent
   ],
+  providers:[CustomPreload],
   imports: [
     BrowserModule,
     HttpModule,
     MailModule,
-    RouterModule.forRoot(ROUTES,{preloadingStrategy:PreloadAllModules})
+    RouterModule.forRoot(ROUTES,{preloadingStrategy:CustomPreload})
   ],
   bootstrap: [
     AppComponent
