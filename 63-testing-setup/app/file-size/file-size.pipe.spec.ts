424a45345fb689bf49e7e94689b03802100dcbe1
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-12 14:49:05 -0400

    init test

--- 63-testing-setup/app/file-size/file-size.pipe.spec.ts
+++ 63-testing-setup/app/file-size/file-size.pipe.spec.ts
@@ -1,6 +1,70 @@
 import { FileSizePipe } from './file-size.pipe';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { Component } from '@angular/core';
+
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule, 
+  platformBrowserDynamicTesting()
+)
 
 describe('FileSizePipe', () => {
+  
+  describe('Isolate FileSizePipe test', () => {
+
+    @Component({
+      template:`Size: {{ size | filesize:suffix }}`
+    })
+
+    class TestComponent{
+      suffix;
+      size = 123456789;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>
+    let el: HTMLElement;
+
+    beforeEach(() => {
+        
+        TestBed.configureTestingModule({
+          declarations:[
+            FileSizePipe,
+            TestComponent
+          ]       
+        });
+        
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        el = fixture.nativeElement;
+
+
+    });
+
+    it('should convert bytes to megabytes 1',()=>{ 
+      fixture.detectChanges();
+      expect(el.textContent).toBe('Size: 117.74MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toBe('Size: 0.98MB');
+    });
+
+    it('should use the default extension when not supplied',()=>{
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+
+    })
+
+    it('should oeverride the extension when supplied',()=>{
+      component.suffix = 'myExt';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74myExt');
+      
+    })
+
+  });
+
 
   describe('Isolate FileSizePipe test', () => {
     
