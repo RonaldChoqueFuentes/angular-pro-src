69f30ac7b54302de33f1c6edf20074dd4518c52a
Ronald Choque Fuentes <Ronald Choque@jalasoft.local>
2018-03-13 18:10:33 -0400

    filter  map

--- 80-observable-store/app/songs/components/songs-favourites/songs-favourites.component.ts
+++ 80-observable-store/app/songs/components/songs-favourites/songs-favourites.component.ts
@@ -3,6 +3,8 @@ import { Store } from '../../../store';
 import { SongsService } from '../../services/songs.service'
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector:'songs-favourites',
@@ -19,17 +21,18 @@ import { Subscription } from 'rxjs/Subscription';
 export class SongsFavouritesComponent implements OnInit, OnDestroy{
     
     favourites$: Observable<any[]>;
-    subscription:Subscription;
+    
 
     constructor(private store:Store,
                 private songsService:SongsService){}
     
     ngOnInit(){
-        this.favourites$ = this.store.select('playlist');
-        this.subscription = this.songsService.getPlaylist$.subscribe();
+        this.favourites$ = this.store.select('playlist')
+        .filter(Boolean)
+        .map(playlist => playlist.filter(track => track.favourite));   
     }
 
     ngOnDestroy(){
-        this.subscription.unsubscribe();
+        
     }
 }
--- 80-observable-store/app/songs/components/songs-listened/songs-listened.component.ts
+++ 80-observable-store/app/songs/components/songs-listened/songs-listened.component.ts
@@ -3,6 +3,8 @@ import { Store } from '../../../store';
 import { SongsService } from '../../services/songs.service'
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector:'songs-listened',
@@ -19,18 +21,15 @@ import { Subscription } from 'rxjs/Subscription';
 export class SongsListenedComponent implements OnInit, OnDestroy{
     
     listened$: Observable<any[]>;
-    subscription:Subscription;
 
     constructor(private store:Store,
                 private songsService:SongsService){}
     
     ngOnInit(){
-        this.listened$ = this.store.select('playlist');
-        this.subscription = this.songsService.getPlaylist$.subscribe();
-
+        this.listened$ = this.store.select('playlist').filter(Boolean)
+        .map(playlist => playlist.filter(track => track.listened));        
     }
 
     ngOnDestroy(){
-        this.subscription.unsubscribe();
     }
 }
